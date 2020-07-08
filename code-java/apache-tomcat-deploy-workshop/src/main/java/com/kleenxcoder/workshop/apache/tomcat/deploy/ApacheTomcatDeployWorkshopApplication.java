66488067cc8fd53e03a7af2e65ce1d4cd5ce2a4c
Peter Stritzinger <peter.satzinger@abf.at>
2020-07-08 09:43:15 +0200

    initial load

---
+++ code-java/apache-tomcat-deploy-workshop/src/main/java/com/kleenxcoder/workshop/apache/tomcat/deploy/ApacheTomcatDeployWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.kleenxcoder.workshop.apache.tomcat.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheTomcatDeployWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheTomcatDeployWorkshopApplication.class, args);
+	}
+
+}
---
+++ code-java/apache-tomcat-deploy-workshop/src/test/java/com/kleenxcoder/workshop/apache/tomcat/deploy/ApacheTomcatDeployWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleenxcoder.workshop.apache.tomcat.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheTomcatDeployWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
